d875dccb148ff5541253303692e74bd0fa0055fc
AparnaMenonS <aparnas1998@gmail.com>
2023-03-16 13:31:43 +0530

    Staging Updated

--- dbtdemo/models/sample/stg/stg_carat_price.sql
+++ dbtdemo/models/sample/stg/stg_carat_price.sql
@@ -3,19 +3,19 @@ with cc as
     select _c0 as id, carat, cut from {{ ref('diamonds_four_cs') }}
 ),
 
- amt as
+amt as
 (
-    select _c0 as id, price from {{ source('default', 'diamonds') }}
+    select _c0 as id, price from {{ ref('diamonds_prices') }}
 ),
 
- final as 
+final as 
 (
     select 
         carat from cc.carat,
         cut from cc.cut
         price from amt.price
 
-    from cc join amt using (id)
+    from cc join amt using (_c0)
 )
 
 select * from final
